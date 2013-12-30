688328fd63bf9feb58ce5ec49dcb3b3662524084
Dave Collier <davetheflashguy@gmail.com>
2013-12-30 18:24:43 -0500

    Router start up sequence

--- public/app.router.js
+++ public/app.router.js
@@ -2,32 +2,52 @@ define(function (require, exports, module) {
 
 	var Backbone = require('backbone');
 
+	require(['ui/menu/menu','ui/home/home', 'ui/samples/samples', 'ui/resume/resume', 'ui/contact/contact'], 
+
+	function(Menu, Home, Samples, Resume, Contact){
+
+		var menu = new Menu();
+			menu.render();
+
+		var home = new Home();
+			home.render();
+
+		var samples = new Samples();
+			samples.render();
+
+		var resume = new Resume();
+			resume.render();
+
+		var contact = new Contact();
+			contact.render();
+
+	});
 	return Backbone.Router.extend({
 		routes: {
-			'': 'init'
+			''			: 'init',
+			'home'		: 'home',
+			'samples'	: 'samples',
+			'resume'	: 'resume',
+			'contact'	: 'contact'
 		},
 		init: function(){
 
-			require(['ui/menu/menu','ui/home/home', 'ui/samples/samples', 'ui/resume/resume', 'ui/contact/contact'], 
-
-			function(Menu, Home, Samples, Resume, Contact){
-
-				var menu = new Menu();
-					menu.render();
-
-				var home = new Home();
-					home.render();
-
-				var samples = new Samples();
-					samples.render();
-
-				var resume = new Resume();
-					resume.render();
-
-				var contact = new Contact();
-					contact.render();
-			});
 		},
+		home: function (){
+
+		},
+		samples: function(){
+
+		},
+		resume: function(){
+
+		},
+		contact: function(){
+
+		},
+		scrollToById: function(id) {
+
+		}
 	});
 
 });
