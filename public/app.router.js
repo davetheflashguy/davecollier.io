907a26c7fdf954e179eeba4795954bee0f77ffb4
Dave Collier <davetheflashguy@gmail.com>
2014-01-02 10:29:19 -0500

    Added some scrolling

--- public/app.router.js
+++ public/app.router.js
@@ -34,19 +34,23 @@ define(function (require, exports, module) {
 
 		},
 		home: function (){
-
+			this.scrollToById($(".home-container"));
 		},
 		samples: function(){
-
+			this.scrollToById($(".samples-container"));
 		},
 		resume: function(){
-
+			this.scrollToById($(".resume-container"));
 		},
 		contact: function(){
-
+			this.scrollToById($(".contact-container"));
 		},
 		scrollToById: function(id) {
-
+			var val = $(id).offset().top - 127;
+			
+			$('html, body').animate({
+			        scrollTop: val
+			}, 2000);
 		}
 	});
 
