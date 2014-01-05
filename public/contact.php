a67d71c5722d92763b49d4560e84e566be736a2c
Dave Collier <davetheflashguy@gmail.com>
2014-01-05 10:31:15 -0500

    Added contact form

---
+++ public/contact.php
@@ -0,0 +1,23 @@
+<?php
+
+$to = 'davetheflashguy@gmail.com';
+
+$subject = 'Website Contact Submission';
+
+$headers = "From: " . strip_tags($_POST['email']) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$message = '<html><body>';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
+$message .= "<tr><td><strong>Subject:</strong> </td><td>" . strip_tags($_POST['subject']) . "</td></tr>";
+$message .= "<tr><td><strong>Message:</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
+$message .= "</table>";
+$message .= "</body></html>";
+
+mail($to, $subject, $message, $headers);
+
+?>
--- public/ui/contact/contact.js
+++ public/ui/contact/contact.js
@@ -4,13 +4,52 @@ define(function (require, exports, module) {
 
 	require('css!./contact');
 	var template = require('html!./contact');
+	
 
 	return BaseView.extend({
+
 		render: function(){
-			
+
 			this.$el.append(template({}));
+			var scope = this;
+
+			$('#submit').click(function(e) {
+    			e.preventDefault();
+    			scope.validateFormOnSubmit();
+			});
 			
+		},
+
+		validateFormOnSubmit: function() {
+
+			var formElements = $('#name, #subject, #email, #message');
+
+			for (i = 0 ; i < formElements.length ; i++)
+			{
+			    if ($(formElements[i]).val().length === 0) {
+			     	$(formElements[i]).addClass('warning');
+			    }
+			}
+
+			if ($('#name').val().length > 0 && $('#subject').val().length > 0 && $('#email').val().length > 0 && $('#message').val().length > 0) {
+				var url = "contact.php"; // the script where you handle the form input.
+
+				return;
+			    $.ajax({
+			           type: "POST",
+			           url: url,
+			           data: $("#contactForm").serialize(), // serializes the form's elements.
+			           success: function(data)
+			           {
+			               //alert(data); // show response from the php script.
+			           }
+			         });
+
+			    return false; // avoid to execute the actual submit of the form.
+			}
+
 		}
+
 	});
 
 });
